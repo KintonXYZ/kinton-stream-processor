b7b327df92f8a618ea2b2cb3106b4ccc6ca16353
Diego Fernández Barrera <bigomby@gmail.com>
2016-10-19 22:19:30 +0200

    :bug: Fix send message to incorrect queue

--- index.js
+++ index.js
@@ -30,74 +30,84 @@ const connection = amqp.createConnection({
   ssl: { enabled: false },
 });
 
+// AQMP queue connection
 connection.on('error', (e) => {
   logger.error('Error from amqp: ', e);
 });
 
+
+// MongoDB on connection
 mongoose.connection.on('connected', () => {
   logger.info('Connected to MongoDB');
 
-  Fleet.find({}, (err, fleets) => {
-    if (err) throw err;
+  connection.on('ready', () => {
+    logger.info('Connected to RabbitMQ');
 
-    for (const fleet of fleets) {
-      logger.info(`Subscription to ${fleet.id}`);
-      connection.queue(fleet.id, { autoDelete: false }, (p) => {
-        p.bind('amq.direct', fleet.id);
+    Fleet.find({}, (err, fleets) => {
+      if (err) throw err;
+
+      for (const fleet of fleets) {
+        logger.info(`Subscription to ${fleet.id}`);
+
+        connection.queue(fleet.id, { autoDelete: false }, (p) => {
+          p.bind('amq.direct', fleet.id);
+        });
 
         connection.queue('stream-processor', { autoDelete: false }, (q) => {
           q.bind(`${fleet.id}.#`);
 
-        // Receive messages
+          // Receive messages
           q.subscribe((msg, headers, deliveryInfo) => {
             const i = deliveryInfo.routingKey.indexOf('.');
+            const fleetKey = deliveryInfo.routingKey.slice(0, i);
+            const topic = deliveryInfo.routingKey.slice(i + 1);
+
+            logger.debug(`Incoming message from fleet: ${fleetKey}/${topic}`);
 
             const message = new Message({
               data: msg.data,
-              topic: deliveryInfo.routingKey.slice(i + 1),
-              fleet: fleet.id,
+              fleet: fleetKey,
+              topic,
             });
 
             message.saveAll().then(() => {
-              logger.debug('Message stored');
+              logger.debug('Message stored ok');
             });
           });
         });
-      });
-    }
-  });
-});
-
-// AQMP queue connection
-connection.on('ready', () => {
-  logger.info('Connected to RabbitMQ');
-
-  // Rethink connected
-  thinky.dbReady().then(() => {
-    logger.info('Connected to RethinkDB');
-
-    // Get notifications for new messages inserted on the database
-    Message.changes().then((feed) => {
-      feed.each((error, doc) => {
-        if (error) throw error;
-
-        // Is a new message
-        if (doc.getOldValue() == null) {
-          // Send the message to the user
-          connection.publish(doc.fleet, doc.data, {
-            messageId: doc.id,
-            timestamp: doc.timestamp.getTime() / 1000,
-            headers: { topic: doc.topic },
-          }, (err) => {
-            logger.error(err);
-          });
-        }
-      });
-    }).error((error) => {
-      logger.error(error);
+      }
     });
-
-    // Connect MongoDB
-    mongoose.connect(`mongodb://${consts.MONGO_HOST}/${consts.MONGO_DB_NAME}`);
   });
 });
+
+// Rethink connected
+thinky.dbReady().then(() => {
+  logger.info('Connected to RethinkDB');
+
+  // Get notifications for new messages inserted on the database
+  Message.changes().then((feed) => {
+    logger.info(`Subscribed to ${feed}`);
+
+    feed.each((error, doc) => {
+      if (error) throw error;
+
+      logger.debug(`Sending message to fleet ${doc.fleet}`);
+      // Is a new message
+      if (doc.getOldValue() == null) {
+        // Send the message to the user
+        connection.publish(doc.fleet, doc.data, {
+          messageId: doc.id,
+          timestamp: doc.timestamp.getTime() / 1000,
+          headers: { topic: doc.topic },
+        }, (err) => {
+          logger.error(err);
+        });
+      }
+    });
+  }).error((error) => {
+    logger.error(error);
+  });
+});
+
+// Connect MongoDB
+mongoose.connect(`mongodb://${consts.MONGO_HOST}/${consts.MONGO_DB_NAME}`);
